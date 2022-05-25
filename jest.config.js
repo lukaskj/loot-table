dbe26e0f6ed283261c7c46d9110c59af607b1b72
Lukas Jacomin <lukasjacomin@gmail.com>
2022-05-25 00:53:02 -0300

    project configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-   transform: { '^.+\\.ts?$': 'ts-jest' },
-   testEnvironment: 'node',
-   testRegex: './tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  clearMocks: true,
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  testEnvironment: "node",
+  testRegex: "./tests/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ["json", "text", "lcov", "clover"],
+  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  maxWorkers: "50%",
+  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
+  watchPathIgnorePatterns: [
+    "\\\\node_modules\\\\",
+    "node_modules"
+  ],
 };
