f06b160389cbaa43a4245e80ffb11519950712d5
Lukas Jacomin <lukasjacomin@gmail.com>
2022-09-21 01:24:27 -0300

    loot table test

--- test/entities/loot-table.test.ts
+++ test/entities/loot-table.test.ts
@@ -3,5 +3,11 @@ describe("Entities", () => {
     it.todo("Should drop one item when calling drop with max equals one");
     it.todo("Should drop more items when calling drop with max more than one");
     it.todo("Should not drop any item when given no loot table");
+
+    describe("fromJSON", () => {
+      it.todo("Should drop one item when calling drop with max equals one");
+      it.todo("Should drop more items when calling drop with max more than one");
+      it.todo("Should not drop any item when given no loot table");
+    });
   });
 });
