fc7bdb442f4b53bec6a9712937d8f6befd69c0d8
Lukas Jacomin <lukasjacomin@gmail.com>
2022-09-20 23:39:46 -0300

    jest coverage output

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["json", "text", "lcov", "clover"],
+  coverageReporters: ['lcov', 'text', 'text-summary', 'html'],
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   maxWorkers: "50%",
   testPathIgnorePatterns: ["\\\\node_modules\\\\", "dist"],
